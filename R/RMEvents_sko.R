ef53231f79f208877a94107679ba31cd4f17a52c
Oliver <soliver@usgs.gov>
2017-06-28 10:40:28 -0500

    changed column indexing. Was indexing by input variable name, not function variable name.

--- R/RMEvents_sko.R
+++ R/RMEvents_sko.R
@@ -44,10 +44,10 @@ RMevents_sko <- function(df,ieHr=6,rainthresh=5.1,rain="rain",time="pdate"){
     }
   }
   
-  rain.events <- aggregate(x = df$rain, by = list(df$event), sum) #find sum of rain in each event
-  start.dates <- aggregate(x = df$pdate, by = list(df$event), min)[,2] #find minimum date for each event
+  rain.events <- aggregate(x = df[[rain]], by = list(df$event), sum) #find sum of rain in each event
+  start.dates <- aggregate(x = df[[time]], by = list(df$event), min)[,2] #find minimum date for each event
   start.dates <- start.dates - timeInterval
-  end.dates <- aggregate(x = df$pdate, by = list(df$event), max)[,2]
+  end.dates <- aggregate(x = df[[time]], by = list(df$event), max)[,2]
   
   out <- data.frame(stormnum = rain.events[,1],
                        StartDate = start.dates,
